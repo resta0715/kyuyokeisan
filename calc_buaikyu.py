080854c5cd69c7c46c29ac22aa7bb93a92d55d54
resta0715 <mdit2416@gmail.com>
2022-06-27 09:35:12 +0900

    namechange

--- calc_buaikyu.py
+++ calc_buaikyu.py
@@ -1,8 +1,10 @@
 buai_list=[0.2,0.05,0.1,0.15]
 uriage_list=[100000,200000,50000,350000]
+name_list=["moto","ryo","aya","miku"]
 for i in range(4):
     buai=buai_list[i]
     uriage=uriage_list[i]
     buaikyuu=buai*uriage
-    print("売上金額",uriage,"歩合率",buai,"歩合給",buaikyuu)
+    name=name_list[i]
+    print(name,"売上金額",uriage,"歩合率",buai,"歩合給",buaikyuu)
 
