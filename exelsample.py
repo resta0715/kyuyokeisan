0ba64e0de2a73438e351562686e6a6b4c62dd50c
resta0715 <mdit2416@gmail.com>
2022-07-04 11:07:18 +0900

    ex1

---
+++ exelsample.py
@@ -0,0 +1,28 @@
+import openpyxl
+book = openpyxl.load_workbook("Book1.xlsx")
+print(len(book.sheetnames))
+sheet=book.get_sheet_by_name('Sheet1')
+resta_data={}
+for row in sheet.rows:
+    #print(row)
+    print(row[1].value)
+    name=row[0].value
+    sd={
+        "name":row[0].value,
+        "working_status":row[1].value,
+        "rank":row[2].value,
+        "position":row[3].value,
+        "hire_date":row[4].value,
+        "time":row[5].value,
+        "zikyu":row[6].value,
+        "parking":row[7].value,
+        "社会保険":row[8].value,
+        "雇用保険":row[9].value,
+        "service_sale":row[10].value,
+        "product_sale":row[11].value
+
+    }
+    resta_data[name]=sd
+    print(sd)
+
+print(resta_data)
