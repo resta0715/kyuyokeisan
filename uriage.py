f488a0577f028dea21a77cb0c8fed92e69ad3c5b
青柳　恭義 <takayoshi_aoyagi@waku-2.com>
2022-07-17 19:01:44 +0900

    fix indent

--- uriage.py
+++ uriage.py
@@ -4,16 +4,15 @@ def get_uriage():
     with open("uriage2022_7.csv") as f:
         reader = csv.DictReader(f)
 
-    uriage={}
-    for row in reader:
-        
-        name=row["スタイリスト名"]
-        kozinuriage={
-            "総売上_内訳_技術":row["総売上_内訳_技術"],
-            "総売上_内訳_店販":row["総売上_内訳_店販"],
-            "総売上_内訳_オプション":row["総売上_内訳_オプション"]
-        }
-        uriage[name]=kozinuriage
-    return uriage
-    
-    
+        uriage={}
+        for row in reader:
+
+            name=row["スタイリスト名"]
+            kozinuriage={
+                "総売上_内訳_技術":row["総売上_内訳_技術"],
+                "総売上_内訳_店販":row["総売上_内訳_店販"],
+                "総売上_内訳_オプション":row["総売上_内訳_オプション"]
+            }
+            uriage[name]=kozinuriage
+        return uriage
+
